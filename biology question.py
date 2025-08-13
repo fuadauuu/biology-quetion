9a20dbb1498e2b03409dd3e5c4abf0b040fde71b
fuad <fuadgg746@gmail.com>
2025-08-13 10:55:59 +0300

    Add files via upload

---
+++ biology question.py
@@ -0,0 +1,27 @@
+print("which one of the ff is voulentary muscle")
+print("a,skeletal muscle")
+print("b, smooth muscle")
+print("c, cardiac muscle")
+print("d, none of the above")
+answer = input("enter your answer (a,b,c,d)")
+if answer=="a":
+    print("a is the correct answer")
+else:
+    print("incorrect, the correct answer is a")
+
+print("which one is largest bone in human")
+print ("a, femur")
+print("b, skull")
+print ("c, ribs")
+print ("d,strunum")
+answer = input("enter your answer (a,b,c,d)")
+if answer=="a":
+    print("a is the correct answer")
+else:
+    print("incorrect, the correct answer is a")
+    
+
+
+
+
+
